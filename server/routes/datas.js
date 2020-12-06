e64a1571faa65f3c3892e7e0c1c2b9165f1857c5
ziho <ziho@kakao.com>
2020-12-06 17:19:47 +0900

    api 수정

--- server/routes/datas.js
+++ server/routes/datas.js
@@ -54,6 +54,12 @@ const uploadAtt = multer({ storage: storageAtt }).single('file');
 
 router.post('/uploadfile', (req, res) => {
 	uploadVideo(req, res, (err) => {
+		let options = {
+			mode: 'text',
+			pythonPath: '/usr/bin/python3',
+			pythonOptions: ['-u'],
+			args: [req.body.studentid, '/home/ubuntu/capstone-Web/' + res.req.file.path],
+		};
 		console.log(res.req.file.path);
 		User.findOneAndUpdate(
 			{ studentId: req.body.studentid },
@@ -66,12 +72,7 @@ router.post('/uploadfile', (req, res) => {
 				return res.status(200).send({ success: true, userInfo });
 			}
 		);
-		let options = {
-			mode: 'text',
-			pythonPath: '/usr/bin/python3',
-			pythonOptions: ['-u'],
-			args: [req.body.studentid, '/home/ubuntu/capstone-Web/' + res.req.file.path],
-		};
+
 		User.find({ studentId: req.body.studentid }, { course: 1 }).exec((err, data) => {
 			console.log(data);
 			for (key of data[0].course) {
@@ -83,8 +84,10 @@ router.post('/uploadfile', (req, res) => {
 				);
 			}
 		});
-		PythonShell.run('/home/ubuntu/faceRecog/videoEmbedding.py', options, (err) => {
+
+		PythonShell.run('/home/ubuntu/faceRecog/videoEmbedding.py', options, (err, result) => {
 			if (err) return res.json({ success: false, err });
+			console.log(result);
 		});
 	});
 });
